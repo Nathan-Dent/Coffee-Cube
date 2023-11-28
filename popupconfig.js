bad6bb5d0f38328d2e61f905f03f828bb6877566
Nathan-Dent <119369216+Nathan-Dent@users.noreply.github.com>
2023-11-28 02:16:06 +0100

    Added Pop-Up menus for all menu items (and site navigation now works)
    Tons of new code here, but mostly the same stuff happening over and over again. My head hurts.

---
+++ popupconfig.js
@@ -0,0 +1,34 @@
+const exit = document.getElementsByClassName('exit');
+
+const allPopUps = document.getElementsByClassName("popup");
+
+function closeWindows() {
+    for (var i = 0; i < allPopUps.length; i++) {
+        allPopUps[i].style.display = 'none';
+    };
+};
+
+function createTrigger(item) {
+    var itemTrigger = document.getElementById(item);
+    var itemPopUp = document.getElementById(item + `-popup`);
+    itemTrigger.addEventListener('click', function (event) {
+        itemPopUp.style.display = 'grid';
+    });
+};
+
+for (var i = 0; i < exit.length; i++) {
+    exit[i].addEventListener('click', closeWindows);
+};
+
+createTrigger('espresso');
+createTrigger('double-espresso');
+createTrigger('americano');
+createTrigger('batch-brew');
+createTrigger('v60');
+createTrigger('macchiato');
+createTrigger('cappuccino');
+createTrigger('latte');
+createTrigger('flat-white');
+createTrigger('mocha');
+createTrigger('dirty-chai');
+createTrigger('irish-coffee');
