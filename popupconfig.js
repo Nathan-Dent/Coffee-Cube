500fea8cece07bb1d3814c4015538ceea884f2f4
Nathan-Dent <119369216+Nathan-Dent@users.noreply.github.com>
2023-12-07 18:15:04 +0100

    Revert "Tried to implement one of Elizabeth's suggestions, didn't work. Reverting."
    This reverts commit 1d610011aa0fe34062308df60818b959c7c2ffcd.

--- popupconfig.js
+++ popupconfig.js
@@ -2,10 +2,6 @@ const exit = document.getElementsByClassName('exit');
 
 const allPopUps = document.getElementsByClassName("popup");
 
-const addToCartButtons = document.getElementsByClassName("add-to-cart");
-
-const quickAdds = document.getElementsByClassName("quick-add");
-
 function closeWindows() {
     for (var i = 0; i < allPopUps.length; i++) {
         allPopUps[i].style.display = 'none';
@@ -22,33 +18,6 @@ function createTrigger(item) {
 
 for (var i = 0; i < exit.length; i++) {
     exit[i].addEventListener('click', closeWindows);
-};
-
-for (var i =0; i < addToCartButtons.length; i++) {
-    addToCartButtons[i].addEventListener('click', closeWindows);
-}
-
-const items = ['espresso',
-'double-espresso',
-'americano',
-'batch-brew',
-'v60',
-'macchiato',
-'cappuccino',
-'latte',
-'flat-white',
-'mocha',
-'dirty-chai',
-'irish-coffee'];
-
-for (var i = 0; i < quickAdds.length; i++) {
-    console.log('THE VALUE OF i IS ' + i);
-    console.log(items[i]);
-    console.log(quickAdds[i]);
-    quickAdds[i].addEventListener('click', function (event) {
-        let itemPopUp = document.getElementById(items[i - 1] + `-popup`);
-        itemPopUp.style.display = 'grid';
-    });
 };
 
 createTrigger('espresso');
